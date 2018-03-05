61789bfb89fef6b6534464600c7e1279cc8c28dc
sravanti298 <36072056+sravanti298@users.noreply.github.com>
2018-03-05 11:20:55 +0530

    Add files via upload

---
+++ method overloading/example1/method_overloading.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Addition
+{
+public:
+void sum(int a, int b)
+{
+cout<<a+b;
+}
+void sum(int a, int b, int c)
+{
+cout<<a+b+c;
+}
+};
+int main()
+{
+
+Addition obj;
+obj.sum(10, 20);
+cout<<endl;
+obj.sum(10, 20, 30);
+}
